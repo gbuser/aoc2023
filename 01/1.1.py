df343438031cd5505dbcbb9a56ab7ba2851e08c7
Gregory <gregory.buser@gmail.com>
2023-12-01 15:53:23 -0600

    revised part 2

--- 01/1.1.py
+++ 01/1.1.py
@@ -1,6 +1,5 @@
 import re
-file = open("01.txt", 'r')
-data = [line for line in file]
+data = [line for line in open("01.txt", 'r')]
 digits = [re.findall(r'\d', line) for line in data]
 digits = [[line[0], line[-1]] for line in digits]
 digits = ([int(''.join(line)) for line in digits])
