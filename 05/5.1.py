f42bf4940f2f559b8acc63eaa6c29464af9143b5
Gregory <gregory.buser@gmail.com>
2023-12-17 20:07:13 -0600

    part 1

--- 05/5.1.py
+++ 05/5.1.py
@@ -2,19 +2,19 @@ file = "input.txt"
 #file = "sample.txt"
 data = [line.split() for line in open(file, 'r')]
 seeds = data.pop(0)
-data.pop(0)
+data.pop(0) # getting rid opf blank line
 seeds = [int(x) for x in seeds[1:]]
-maps = []
+maps = [] #maps is a list of dictionaries of the mapping data
 for line in data:
     if line and line[0][0].isalpha():
-        print(line[0].split('-')[2])
         new_map = []
     elif line == []:
         maps.append(new_map.copy())
     else:
         new_map.append({'start': int(line[1]), 'end': int(line[1]) + int(line[2]), 'map_to': int(line[0])})
-maps.append(new_map.copy())
+maps.append(new_map.copy()) #o/w one map short!
 def next_mapping(seed, n):
+    # takes a source, a map and maps to next
     for item in maps[n]:
         if seed in range(item['start'], item['end']):
             return (seed + item['map_to'] - item['start'])
