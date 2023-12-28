c6a94b08aaec5e3db255f9e40c4387c0a423c2cd
Gregory <gregory.buser@gmail.com>
2023-12-28 17:51:05 -0600

    revised part 1 changed to set

--- 21/21.py
+++ 21/21.py
@@ -3,24 +3,24 @@ data = open(file, 'r').readlines()
 data = [[char for char in line.strip()]  for line in data]
 possible_gardens = set() #where you could be after each step
 width, height = len(data[0]), len(data)
-gardens = []
+gardens = set()
 rocks = []
 for y in range(height):
     for x in range(width):
         match data[y][x]:
             case '.':
-                gardens.append((x,y))
+                gardens.add((x,y))
             case '#':
                 rocks.append((x,y))
             case 'S':
                 start = ((x,y))
-                gardens.append(start)
+                gardens.add(start)
                 possible_gardens.add(start)
 
-def take_a_step(possible):
+def take_a_step(possible_gardens):
     #returns all valid garden locations after one more step
     garden_neighbors = set()
-    for location in possible:
+    for location in possible_gardens:
         new_neighbors = find_garden_neighbors(*location)
         garden_neighbors |= new_neighbors
     return garden_neighbors
